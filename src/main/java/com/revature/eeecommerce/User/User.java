804a3aeca75af2f5dd6ae282b88f9fa91639987a
Amsal Kassam <aakassam1@yahoo.com>
2024-07-24 16:43:56 -0500

    added User model

--- src/main/java/com/revature/eeecommerce/User/User.java
+++ src/main/java/com/revature/eeecommerce/User/User.java
@@ -1,4 +1,58 @@
 package com.revature.eeecommerce.User;
 
 public class User {
+    int userID;
+    String firstName;
+    String lastName;
+    String address;
+    String email;
+    String password;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
