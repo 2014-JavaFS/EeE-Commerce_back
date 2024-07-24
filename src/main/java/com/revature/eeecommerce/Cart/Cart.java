60d89ff00aacf2160bca6e948ef932b7c2ec0a05
Connie Angeley <connieangeley@gmail.com>
2024-07-24 17:35:46 -0500

    Created cart model

--- src/main/java/com/revature/eeecommerce/Cart/Cart.java
+++ src/main/java/com/revature/eeecommerce/Cart/Cart.java
@@ -1,5 +1,48 @@
 package com.revature.eeecommerce.Cart;
 
 public class Cart {
+    private int userId;
+    private int productId;
+    private int count;
 
+    public Cart(){}
+
+    public Cart(int userId, int productId, int count) {
+        this.userId = userId;
+        this.productId = productId;
+        this.count = count;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "userId=" + userId +
+                ", productId=" + productId +
+                ", count=" + count +
+                '}';
+    }
 }
