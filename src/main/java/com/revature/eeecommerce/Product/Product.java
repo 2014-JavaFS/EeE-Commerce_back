211ab35a1bb7b592b8ae9a50fda168f7a333750a
Amsal Kassam <aakassam1@yahoo.com>
2024-07-25 16:41:17 -0500

    added quantity field to product

--- src/main/java/com/revature/eeecommerce/Product/Product.java
+++ src/main/java/com/revature/eeecommerce/Product/Product.java
@@ -25,4 +25,6 @@ public class Product {
     @Column(nullable = false)
     private String name;
     private String description;
+    @Column(nullable = false, columnDefinition = "integer default 0")
+    private int quantity;
 }
