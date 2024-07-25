77f172035ce51f999bc5c61b4666183a53ef29e2
rubensamf <ruben146@revature.net>
2024-07-25 14:36:59 -0500

    Added annotations

--- src/main/java/com/revature/eeecommerce/Order/Order.java
+++ src/main/java/com/revature/eeecommerce/Order/Order.java
@@ -1,15 +1,16 @@
 package com.revature.eeecommerce.Order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
     private int orderId;
     private int userId;
     private Timestamp date;
-
-    public Order(int orderId, int userId, Timestamp date) {
-        this.orderId = orderId;
-        this.userId = userId;
-        this.date = date;
-    }
 }
