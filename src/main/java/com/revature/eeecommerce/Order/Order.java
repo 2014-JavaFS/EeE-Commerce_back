c41b54c747c7e2b84f05fcd97838d609d01e697e
rubensamf <ruben146@revature.net>
2024-07-25 13:59:08 -0500

    Created fields, constructor, getters/setters for Order.java

--- src/main/java/com/revature/eeecommerce/Order/Order.java
+++ src/main/java/com/revature/eeecommerce/Order/Order.java
@@ -1,4 +1,39 @@
 package com.revature.eeecommerce.Order;
 
+import java.sql.Timestamp;
+
 public class Order {
+    private int orderId;
+    private int userId;
+    private Timestamp date;
+
+    public Order(int orderId, int userId, Timestamp date) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.date = date;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
 }
