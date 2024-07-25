1d0db8b2b3a85c2b12158698d011458bf4805b93
Amsal Kassam <aakassam1@yahoo.com>
2024-07-25 13:57:02 -0500

    updated user model, removed application.properties, commented out part of pom.xml

--- src/main/java/com/revature/eeecommerce/User/User.java
+++ src/main/java/com/revature/eeecommerce/User/User.java
@@ -1,58 +1,35 @@
 package com.revature.eeecommerce.User;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="users")
 public class User {
-    int userID;
-    String firstName;
-    String lastName;
-    String address;
-    String email;
-    String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
 
-    public int getUserID() {
-        return userID;
-    }
+    private String firstName;
+    private String lastName;
+    private String address;
+    @Column(unique=true, nullable=false)
+    private String email;
+    @Column(nullable=false)
+    private String password;
+    @Column(name="user_type", columnDefinition = "varchar(10) default 'CUSTOMER'")
+    @Enumerated(EnumType.STRING)
+    private userType userType;
 
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public enum userType {
+        CUSTOMER, EMPLOYEE
     }
 }
