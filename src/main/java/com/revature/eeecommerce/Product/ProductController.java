0b4757c3dc5019aa63b1f00f4a7c09b7f3781a43
aramsing <aramsin2@hotmail.com>
2024-07-26 16:45:47 -0500

    Gathered use cases for Products and labeled which methods to use from Repositories, deleted unneeded files after merge from main

--- src/main/java/com/revature/eeecommerce/Product/ProductController.java
+++ src/main/java/com/revature/eeecommerce/Product/ProductController.java
@@ -3,11 +3,16 @@ package com.revature.eeecommerce.Product;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
+/** CONTROLLER CLASS DOCUMENTATION
  * @author Arjun Ramsinghani
+ * The Controller class will interact with the front end part of our application and send the data coming through there through our Service class for processing and validation.
+ * Instructions on how each method should interact are within each method call.
  */
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    // find all products
+    // add products
+    // remove products (BIG MAYBE)
 }
--- src/main/java/com/revature/eeecommerce/Product/ProductService.java
+++ src/main/java/com/revature/eeecommerce/Product/ProductService.java
@@ -2,10 +2,16 @@ package com.revature.eeecommerce.Product;
 
 import org.springframework.stereotype.Service;
 
-/**
+/** SERVICE CLASS DOCUMENTATION
  * @author Arjun Ramsinghani
+ * The Service class is used to define the business logic coming to the backend by use of Spring Boot and will communicate with the database through Spring Data.
+ * Instructions on how each method should interact are within each method call.
  */
 
 @Service
 public class ProductService {
+    // find all products
+    // save products
+    // remove products by an id (BIG MAYBE)
+    // calculate price
 }
