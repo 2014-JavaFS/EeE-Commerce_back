1186f5ca5e0ee96f258c677a8472014e6f8e7159
Amsal Kassam <aakassam1@yahoo.com>
2024-07-31 10:00:16 -0500

    Finished Service

--- src/main/java/com/revature/eeecommerce/User/UserController.java
+++ src/main/java/com/revature/eeecommerce/User/UserController.java
@@ -1,10 +1,30 @@
 package com.revature.eeecommerce.User;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    private ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.create(user));
+    }
+
+
 }
--- src/main/java/com/revature/eeecommerce/User/UserService.java
+++ src/main/java/com/revature/eeecommerce/User/UserService.java
@@ -1,7 +1,50 @@
 package com.revature.eeecommerce.User;
 
+import org.hibernate.exception.DataException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.util.List;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    /**
+     * Searches for a User by email and password
+     *
+     * @param email - String
+     * @param password - String
+     * @return User object if found, else null and throws AuthException
+     */
+    public User findByEmailAndPassword(String email, String password) throws AuthenticationException {
+        return userRepository.findbyEmailAndPassword(email, password).orElseThrow(() -> new AuthenticationException());
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(int id)throws RuntimeException {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Nothing in the database with ID of " + id));
+    }
+
+    public boolean update(User user) {
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean delete(int id) {
+        userRepository.deleteById(id);
+        return true;
+    }
 }
