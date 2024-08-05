4866298f636647a732063a60f2976ef4304d90f3
Amsal Kassam <aakassam1@yahoo.com>
2024-08-05 16:04:07 -0500

    renamed main runner to play nice with git

--- src/main/java/com/revature/eeecommerce/EeecommerceApplication.java
+++ src/main/java/com/revature/eeecommerce/EeecommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EeEcommerceApplication {
+public class EeecommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EeEcommerceApplication.class, args);
+		SpringApplication.run(EeecommerceApplication.class, args);
 	}
 
 }
